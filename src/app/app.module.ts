9bcf074e54e1cd6c8d865e4081fb67e2bfea1465
Ginelle-19 <101726053+Ginelle-19@users.noreply.github.com>
2023-01-31 13:41:54 +0800

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { TodoComponent } from './todo/todo.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 //Step 4 - use Routes
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'todo', component: TodoComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ProjectsComponent },
 ];
 //Step 5 - use RouterModule
 @NgModule({
